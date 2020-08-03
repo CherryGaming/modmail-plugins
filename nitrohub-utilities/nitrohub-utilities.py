c2203a0c8584b712d6295887af80b8fab2d10185
Fumple <44124242+fumple@users.noreply.github.com>
2020-08-04 01:39:19 +0200

    [nitrohub-utilities] Final version

--- nitrohub-utilities/nitrohub-utilities.py
+++ nitrohub-utilities/nitrohub-utilities.py
@@ -19,7 +19,7 @@ class NitroHub(commands.Cog):
         except KeyError:
             await ctx.send(embed=discord.Embed(description="There isn't a partner role set\nAdmins can set it with `partnerrole set [role]`", color=0x9b59b6))
 
-    @partner_role.command()
+    @partner_role.command(name="set")
     @checks.has_permissions(PermissionLevel.ADMIN)
     async def partner_role_set(self, ctx, *, role: discord.Role):
         """Sets the partner role"""
@@ -28,7 +28,7 @@ class NitroHub(commands.Cog):
             {'$set': {'nitrohub': {'partner': str(role.id)}}},
             upsert=True
         )
-        await ctx.send(embed=discord.Embed(description="The partner role is now "+role, color=0x9b59b6))
+        await ctx.send(embed=discord.Embed(description="The partner role is now "+role.mention, color=0x9b59b6))
     
     @commands.command(aliases=["apartner", "addpartner"])
     @checks.has_permissions(PermissionLevel.SUPPORTER)
@@ -37,12 +37,11 @@ class NitroHub(commands.Cog):
         """Adds the partner role to the thread recipient"""
         try:
             roles = ((await self.db.find_one({'_id': 'config'})) or {})['nitrohub']
-            async with ctx.typing():
-                try:
-                    await ctx.thread.recipient.add_roles(roles['partner'], reason="Role added by "+ctx.author.display_name+" ("+ctx.author.username+"#"+ctx.author.discriminator+") ["+ctx.author.id+"]") 
-                    await ctx.send(embed=discord.Embed(description="Added <@&"+roles['partner']+"> to "+ctx.thread.recipient, color=0x9b59b6))
-                except:
-                    await ctx.send(embed=discord.Embed(description="Failed to add <@&"+roles['partner']+"> to "+ctx.thread.recipient, color=0xff0000))
+            try:
+                await ctx.guild.get_member(ctx.thread.recipient.id).add_roles(ctx.guild.get_role(int(roles['partner'])), reason="Role added by "+ctx.author.display_name+" ("+ctx.author.name+"#"+ctx.author.discriminator+") ["+str(ctx.author.id)+"]")
+                await ctx.send(embed=discord.Embed(description="Added <@&"+roles['partner']+"> to "+ctx.thread.recipient.mention, color=0x9b59b6))
+            except discord.Forbidden:
+                await ctx.send(embed=discord.Embed(description="Failed to add <@&"+roles['partner']+"> to "+ctx.thread.recipient.mention, color=0xff0000))
         except KeyError:
             await ctx.send(embed=discord.Embed(description="Partner role not found", color=0xff0000))
     
@@ -53,12 +52,11 @@ class NitroHub(commands.Cog):
         """Removes the partner role from the thread recipient"""
         try:
             roles = ((await self.db.find_one({'_id': 'config'})) or {})['nitrohub']
-            async with ctx.typing():
-                try:
-                    await ctx.thread.recipient.add_roles(roles['partner'], reason="Role removed by "+ctx.author.display_name+" ("+ctx.author.username+"#"+ctx.author.discriminator+") ["+ctx.author.id+"]") 
-                    await ctx.send(embed=discord.Embed(description="Removed <@&"+roles['partner']+"> from "+ctx.thread.recipient, color=0x9b59b6))
-                except:
-                    await ctx.send(embed=discord.Embed(description="Failed to remove <@&"+roles['partner']+"> from "+ctx.thread.recipient, color=0xff0000))
+            try:
+                await ctx.guild.get_member(ctx.thread.recipient.id).remove_roles(ctx.guild.get_role(int(roles['partner'])), reason="Role removed by "+ctx.author.display_name+" ("+ctx.author.name+"#"+ctx.author.discriminator+") ["+str(ctx.author.id)+"]") 
+                await ctx.send(embed=discord.Embed(description="Removed <@&"+roles['partner']+"> from "+ctx.thread.recipient.mention, color=0x9b59b6))
+            except discord.Forbidden:
+                await ctx.send(embed=discord.Embed(description="Failed to remove <@&"+roles['partner']+"> from "+ctx.thread.recipient.mention, color=0xff0000))
         except KeyError:
             await ctx.send(embed=discord.Embed(description="Partner role not found", color=0xff0000))
 
